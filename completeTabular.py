9400da0747e3d2ce93d87b2733c460cbc0c88058
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-09-13 17:24:27 +0200

    This is the true init  Changes to be committed:

--- completeTabular.py
+++ completeTabular.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
-print("Hello world!")
+def main():
+    groundTruth = pd.read_csv("data/GroundTruth.csv")
+    metaData = pd.read_csv("data/metadata.csv")
+    columns = ["attribution", "copyright_license", "acquisition_day", "benign_malignant", "clin_size_long_diam_mm", "concomitant_biopsy", 
+           "diagnosis","diagnosis_confirm_type","family_hx_mm","fitzpatrick_skin_type","image_type","lesion_id","mel_class",
+           "mel_mitotic_index","mel_thick_mm","mel_type","mel_ulcer","melanocytic","nevus_type","patient_id","personal_hx_mm",
+           "pixels_x","pixels_y", "dermoscopic_type"]
+    metaData.drop(columns, axis=1, inplace = True)
+    result = pd.merge(metaData, groundTruth, left_on='isic_id', right_on='image', how='inner')
+    result.drop("image", axis=1, inplace = True)
+    result.dropna(inplace = True)
+    print(result.info())
+    result.to_csv('skinData.csv', index=False)
+
+if __name__ == '__main__' : main()
