e9c2d7597c71b19b8c995a051a43854d2340e2e5
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-09-13 18:39:19 +0200

    Es un virus  Changes to be committed:

--- completeTabular.py
+++ completeTabular.py
@@ -1,9 +1,25 @@
 import pandas as pd
+import os
+
+def delete_photos(file_list, file_directory):
+    for filename in file_list:
+        file_path = os.path.join(file_directory, filename + ".jpg")
+        try:
+            os.remove(file_path)
+            print(f"Deleted {filename}")
+        except FileNotFoundError:
+            print(f"{filename} not found.")
+        except PermissionError:
+            print(f"No permission to delete {filename}.")
+        except Exception as e:
+            print(f"Error deleting {filename}: {str(e)}")
 
 def main():
     #Load both .csv
     groundTruth = pd.read_csv("data/GroundTruth.csv")
     metaData = pd.read_csv("data/metadata.csv")
+
+    print(metaData.info())
 
     #Remove unneccesary columns
     columns = ["attribution", "copyright_license", "acquisition_day", "benign_malignant", "clin_size_long_diam_mm", "concomitant_biopsy", 
@@ -17,8 +33,12 @@ def main():
 
     #Clean unneccesary data
     result.drop("image", axis=1, inplace = True)
-    result.dropna(inplace = True)
 
+    rows_with_nan = result[result.isna().any(axis=1)]
+    pictures_to_be_removed = rows_with_nan["isic_id"].to_list()
+    delete_photos(pictures_to_be_removed, "data/images/")
+
+    result.dropna(inplace = True)
     #Write the result
     print(result.info())
     result.to_csv('skinData.csv', index=False)
