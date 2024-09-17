14e72b0e458c76eb174576d6b2d26485ce5ac6b5
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-09-17 18:53:42 +0200

    asdsad  Changes to be committed:

--- TheAmazingSpiderman/SpiderMans/__init__.py
+++ TheAmazingSpiderman/SpiderMans/__init__.py
@@ -3,12 +3,12 @@ import subprocess
 def ejecutar_mitmproxy():
     try:
         # Comando para ejecutar mitmproxy en modo regular y escuchar en el puerto 8080
-        comando = ['mitmproxy', '--mode', 'regular', '--listen-port', '8080']
+        comando = ['mitmproxy', '--mode', 'regular', '--listen-port', '8080', '-s', 'bloqueador_js.py']
 
         # Ejecuta el comando en una subproceso
         proceso = subprocess.Popen(comando, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        print("mitmproxy iniciado en el puerto 8080")
+        print("mitmproxy iniciado en el puerto 8080 con bloqueo de JavaScript")
 
         # Puedes esperar a que el proceso termine o hacer que el script continúe.
         proceso.wait()
@@ -16,5 +16,4 @@ def ejecutar_mitmproxy():
     except Exception as e:
         print(f"Error al iniciar mitmproxy: {e}")
 
-if __name__ == "__main__":
-    ejecutar_mitmproxy()
+ejecutar_mitmproxy()
