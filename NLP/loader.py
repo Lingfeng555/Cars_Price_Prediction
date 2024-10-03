418a50a691c6f09a7b77b76873fb847acf555995
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-10-03 09:23:40 +0200

    Organizacion

--- NLP/loader.py
+++ NLP/loader.py
@@ -1,28 +1,37 @@
 import os
 import pandas as pd
 
-def merge_csv_files_from_folder(folder_path):
-    # Lista para almacenar los DataFrames
-    df_list = []
+class Loader:
+    train_path = "final_data/train"
+    test_path = "final_data/test"
+    original_path = "final_data/original"
 
-    # Recorrer todos los archivos en la carpeta
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.csv'):
-            # Obtener la ruta completa del archivo
-            file_path = os.path.join(folder_path, filename)
-            
-            # Leer el archivo CSV y agregarlo a la lista de DataFrames
-            df = pd.read_csv(file_path)
-            df_list.append(df)
+    def __load_data(self, folder_path):
+        # Lista para almacenar los DataFrames
+        df_list = []
 
-    # Concatenar todos los DataFrames en uno solo
-    if df_list:
-        merged_df = pd.concat(df_list, ignore_index=True)
-        return merged_df
-    else:
-        print("No se encontraron archivos CSV en la carpeta.")
-        return pd.DataFrame()  
+        # Recorrer todos los archivos en la carpeta
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.csv'):
+                # Obtener la ruta completa del archivo
+                file_path = os.path.join(folder_path, filename)
+                
+                # Leer el archivo CSV y agregarlo a la lista de DataFrames
+                df = pd.read_csv(file_path)
+                df_list.append(df)
 
-data = merge_csv_files_from_folder("NLP/test_data")
-print(len(data))
-print(data["transmissionTypeId"].value_counts())
+        # Concatenar todos los DataFrames en uno solo
+        if df_list:
+            merged_df = pd.concat(df_list, ignore_index=True)
+            return merged_df
+        else:
+            print("No se encontraron archivos CSV en la carpeta.")
+            return pd.DataFrame()
+        
+    def load_train(self): return self.__load_data(self.train_path)
+
+    def load_test(self): return self.__load_data(self.test_path)
+
+    def load_original(self): return self.__load_data(self.original_path)
+
+    def load__all(self): return {"original": self.load_original(), "train": self.load_train(), "test": self.load_test()}
