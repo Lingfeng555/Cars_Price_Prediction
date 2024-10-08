5277bef9fbacf87c9fab9c1f59281db5ad56c1be
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-10-08 10:51:54 +0200

    CNN con pytorch va a ser Changes to be committed:

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-from NLP import *
+#from NLP import *
+import torch
 
 if __name__ == '__main__':
     #train = Loader.load_train()
-    model = DescModel()
+    #model = DescModel()
+    # Verifica si CUDA está disponible
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(torch.cuda.is_available())
+    print(torch.cuda.current_device())
+    print(torch.cuda.get_device_name(torch.cuda.current_device()))
+    print(f"Using device: {device}")
+
