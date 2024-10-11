295449dd772ca26b58b998dcf9e59f10e94336ac
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-10-11 15:12:29 +0200

    Quitando duplicados

--- test.py
+++ test.py
@@ -5,14 +5,15 @@ print("TF_ENABLE_ONEDNN_OPTS:", os.getenv('TF_ENABLE_ONEDNN_OPTS'))
 from NLP import *
 
 if __name__ == '__main__':
-    test = Loader.load_train()
+    test = Loader.load_test()
+    print(len(test))
     model = DescModel()
 
-    prediction = model.predict(test)
+    #prediction = model.predict(test)
 
-    real_price = test["price"]
-    diff = np.mean(abs((real_price - prediction) / real_price))
-    print(f"Hay un MAPE de {diff * 100}%")
+    #real_price = test["price"]
+    #diff = np.mean(abs((real_price - prediction) / real_price))
+    #print(f"Hay un MAPE de {diff * 100}%")
 
 
 
