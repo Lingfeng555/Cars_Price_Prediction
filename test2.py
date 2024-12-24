8621a354a57a2cb30b9ddf553ec70a99ff29a6d6
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-12-24 15:27:26 +0100

    Mas orden

--- test2.py
+++
@@ -1,5 +0,0 @@
-import cuml.ensemble
-import pandas as pd
-from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVC
-from cuml.ensemble import RandomForestClassifier, DecisionTreeClassifier
